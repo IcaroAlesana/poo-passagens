bf3b1cea7b69785b05e1715e01e88fb120345ab8
Icaro Cristofolini <icaroaj@hotmail.com>
2018-10-01 23:46:20 -0300

    B resolvido

---
+++ passagens/src/udesc/cct/poo/passagem/controle/ControleMarcarPassagem.java
@@ -0,0 +1,116 @@
+package udesc.cct.poo.passagem.controle;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import udesc.cct.poo.passagem.modelos.Assento;
+import udesc.cct.poo.passagem.modelos.Local;
+import udesc.cct.poo.passagem.modelos.Parada;
+import udesc.cct.poo.passagem.modelos.Viagem;
+import udesc.cct.poo.passagem.servicos.ServicoDeLocais;
+import udesc.cct.poo.passagem.servicos.ServicoDePassagens;
+import udesc.cct.poo.passagem.servicos.ServicoDeViagens;
+
+public class ControleMarcarPassagem {
+
+    private Scanner scanner;
+    private ServicoDeLocais servicoDeLocais;
+    private ServicoDeViagens servicoDeViagens;
+    private ServicoDePassagens servicoDePassagens;
+
+    public ControleMarcarPassagem(Scanner scanner, ServicoDeLocais servicoDeLocais, ServicoDeViagens servicoDeViagens, ServicoDePassagens servicoDePassagens) {
+        this.scanner = scanner;
+        this.servicoDeLocais = servicoDeLocais;
+        this.servicoDeViagens = servicoDeViagens;
+        this.servicoDePassagens = servicoDePassagens;
+    }
+
+    public void iniciar() {
+        Local origem = escolherOrigem();
+        Local destino = escolherDestino();
+        Viagem escolhida = acharUmaViagemPorOrigemDestino(origem, destino);
+        Assento assento = escolherAssento(escolhida);
+        assento.reservar();
+
+        ArrayList<Assento> assentos = escolhida.getAssentos();
+        this.listarAssentos(assentos);
+    }
+
+    public Viagem acharUmaViagemPorOrigemDestino(Local origem, Local destino) {
+        ArrayList<Viagem> viagens = servicoDeViagens.getTodasAsViagensPorOrigemDestino(origem, destino);
+        listarViagens(viagens, origem, destino);
+        System.out.println("escolha um Onibus:");
+        int viagensIdx = this.scanner.nextInt();
+        Viagem escolhida = viagens.get(viagensIdx - 1);
+
+        return escolhida;
+    }
+
+    public void listarLocais(ArrayList<Local> locais) {
+
+        for (int i = 0; i < locais.size(); i++) {
+            int idx = i + 1;
+            Local l = locais.get(i);
+            String nome = l.getNome();
+            System.out.println(idx + ") " + nome);
+        }
+    }
+
+    public void listarViagens(ArrayList<Viagem> viagens, Local origem, Local destino) {
+
+        for (int i = 0; i < viagens.size(); i++) {
+            int idx = i + 1;
+            Viagem v = viagens.get(i);
+            Parada embarque = v.getParadaPorNome(origem.getNome());
+            Parada desembarque = v.getParadaPorNome(destino.getNome());
+            System.out.println(idx + ") " + embarque.getInfo() + " " + desembarque.getInfo());
+        }
+    }
+
+    public void listarAssentos(ArrayList<Assento> assentos) {
+        for (int i = 0; i < assentos.size(); i+=2) {
+            Assento a = assentos.get(i);
+            Assento b = assentos.get(i+1);
+            System.out.println(a.toString() + " | " + b.toString());
+        }
+    }
+
+    public Local escolherOrigem() {
+        ArrayList<Local> locais = servicoDeLocais.getTodosOsLocais();
+        listarLocais(locais);
+        System.out.println("escolha uma ORIGEM:");
+        int origemIdx = this.scanner.nextInt();
+        Local origem = locais.get(origemIdx - 1);
+
+        System.out.println("------------------------------------");
+        System.out.println("ORIGEM:" + origem.getNome());
+        System.out.println("------------------------------------");
+
+        return origem;
+    }
+
+    public Local escolherDestino() {
+        ArrayList<Local> locais = servicoDeLocais.getTodosOsLocais();
+        this.listarLocais(locais);
+        System.out.println("escolha um DESTINO:");
+        int destinoIdx = this.scanner.nextInt();
+        Local destino = locais.get(destinoIdx - 1);
+
+        System.out.println("------------------------------------");
+        System.out.println("DESTINO:" + destino.getNome());
+        System.out.println("------------------------------------");
+
+        return destino;
+    }
+
+    public Assento escolherAssento(Viagem viagem) {
+        ArrayList<Assento> assentos = viagem.getAssentos();
+        this.listarAssentos(assentos);
+        System.out.println("escolha um assento:");
+        int assentoIdx = this.scanner.nextInt();
+        Assento assento = assentos.get(assentoIdx - 1);
+
+        return assento;
+    }
+
+}
+
---
+++ passagens/src/udesc/cct/poo/passagem/modelos/Assento.java
@@ -0,0 +1,38 @@
+package udesc.cct.poo.passagem.modelos;
+public class Assento{
+    public static int LEITO = 0;
+    public static int SEMILEITO = 1;
+    public static int CONVENCIONAL = 2;
+
+    private int tipo;
+    private int numero;
+    private boolean reservado;
+
+    public Assento(int tipo, int numero){
+        this.reservado = false;
+        this.tipo = tipo;
+        this.numero = numero;
+    }
+
+    public int getTipo(){
+        return this.tipo;
+    }
+    public int getNumero(){
+        return this.numero;
+    }
+    public boolean estaDesocupado(){
+        return !this.reservado;
+    }
+    public void reservar(){
+        this.reservado = true;
+    }
+
+    public String toString(){
+        String tp, free;
+        if (this.getTipo() == 0) tp = "Leito";
+        else tp = (getTipo() == 1) ? "Semi-leito" : "Convencional";
+        free = (this.reservado == false) ? "Desocupado" : "Ocupado";
+
+        return (this.getNumero() + ") " + free + " Tipo: " + tp);
+    }
+}
